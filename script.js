11501c99fa56c1277658c7ab5a9807d6b284226f
Christopher Holmes <holmes.chris94@yahoo.com>
2020-06-18 22:04:54 -0400

    Layout created

---
+++ script.js
@@ -0,0 +1,5 @@
+//1. Display current time using Moment.js
+//2. Click event to save text are content to local storage
+//3. When app opens, display content for each time block from local storage.
+    //It can be done with a loop (more efficient) or by targeting each time block separately.
+//4. Change time block background-color depending on the current hour (past, present, and future).
